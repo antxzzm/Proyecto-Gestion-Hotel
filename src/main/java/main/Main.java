f088a14fedce0e69987c3c18804ca372db3171c3
Anthony <anthonymelendres69@gmail.com>
2025-07-09 23:18:55 -0500

    "Agregadas clase main como punto de entrada del sistema"

---
+++ src/main/java/main/Main.java
@@ -0,0 +1,13 @@
+package main;
+
+import vista.Login;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Login ventana = new Login();
+        ventana.setVisible(true);
+        ventana.setLocationRelativeTo(null);
+    }
+
+}
